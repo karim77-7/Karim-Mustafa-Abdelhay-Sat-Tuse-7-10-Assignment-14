1f331f56b8b9ee798dc62ffaaaddd3f930b753e6
Karim Mustafa <appie.34@Karims-MacBook-Pro.local>
2026-02-07 02:14:14 +0200

    first commit

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,11 @@
 import { Routes } from '@angular/router';
+import { Home } from './components/home/home';
+import { Blog } from './components/blog/blog';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path: '', redirectTo: 'home', pathMatch: 'full'},
+    {path:"home",component:Home , title: 'Home'},
+    {path:"blog",component:Blog , title: 'Blog'},
+    {path: '**', redirectTo: 'home', pathMatch: 'full'}
+
+];
--- src/app/app.ts
+++ src/app/app.ts
@@ -1,9 +1,13 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { Navbar } from "./components/navbar/navbar";
+import { Footer } from "./components/footer/footer";
+import { Home } from "./components/home/home";
+import { Blog } from "./components/blog/blog";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, Navbar, Footer, Home, Blog],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
